456e62d9863d3a3ea4a8818db9131322e7c70e2e
minniejung <94359917+minniejung@users.noreply.github.com>
2022-01-13 14:57:50 +0100

    First commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,13 @@
-
-
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
-mongoose.connection.on("connected", () => console.log("yay mongodb connected :)"));
+mongoose.connection.on("connected", () =>
+  console.log(`Yay! mongodb connected : http://127.0.0.1:${PORT}`)
+);
 
 mongoose.connection.on("error", () => console.log("nay db error sorry :("));
-
