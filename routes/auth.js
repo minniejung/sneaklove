fc7dc71fd073d34c9c07f6c1be865bac94a7edcd
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-13 17:50:45 +0100

    signin

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const router = new express.Router();
 const User = require("./../models/User");
@@ -17,16 +18,46 @@ router.post("/auth/signup", async (req, res, next) => {
     console.log(newUser);
     const foundUser = await User.findOne({ email: newUser.email });
     if (foundUser) {
+      req.flash("error", "Email already regristered");
       res.redirect("/auth/signin");
     } else {
       const hashedPwd = bcrypt.hashSync(newUser.password, 10);
       newUser.password = hashedPwd;
       await User.create(newUser);
+      req.flash("success", "succeed :)");
       res.redirect("/auth/signin");
     }
   } catch (error) {
     next(error);
   }
 });
+router.post("/auth/signin", async (req, res) => {
 
+  try{
+    const {email, password} = req.body
+  
+   const foundedUser = await User.findOne({email : email})
+   console.log(foundedUser)
+   if(!foundedUser){
+    req.flash("error", "Email not regristered");
+    res.redirect("/auth/signup");
+   }else{
+     const pwd = bcrypt.compareSync(password, foundedUser.password)
+     if(pwd){
+      req.flash("success", "Welcome :)");
+       const objectUser = foundedUser.toObject()
+       delete objectUser.password
+       req.session.currentUser = objectUser;
+       res.redirect("/")
+
+     }else{
+      req.flash("warning", "wrong password");
+      res.redirect("/auth/signin")
+     }
+   }
+  }catch(err){
+    console.error(err)
+  }
+
+});
 module.exports = router;
