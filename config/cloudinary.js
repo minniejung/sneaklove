a10d64077da17d476d581958f9cbd89445bd7d6d
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-14 11:46:57 +0100

    create prod add

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,1 +1,21 @@
-module.exports = "foo";
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+// giving access to your cloudinary account
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+// cloudinary : SAAS platform : specialized in images hosting (tools : metadata, image analyzing ...)
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    allowed_formats: ['jpg', 'png'],
+    folder: 'sneaklove' // The name of the folder in cloudinary
+    // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
+  }
+});
+module.exports =  multer({ storage });
+// a middleware designed to parse file from requests and associate to req.file
--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,38 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
+const tagsModel = require('../models/Tag')
+const sneakerModel = require('../models/Sneaker')
+const protectPrivateRoute = require("../middlewares/protectPrivateRoute");
+const uploader = require("./../config/cloudinary");
 
+router.get('/prod-add',protectPrivateRoute, async (req, res)=>{
+    try{
+        res.render('products_add', {tags: await tagsModel.find()});
+    }catch(err){
+        console.error(err)
+    }
+    
+})
+router.post('/prod-add',uploader.single("image"), async(req, res, next)=>{
+    console.log(req.file)
+    try{
+        const {name, ref, size, description, price,category, id_tags } = req.body;
+        let  newPicture = req.file.path;
+      const updated = await sneakerModel.create(
+        {
+          name,
+          ref,
+          description,
+          size,
+          price,
+          category,
+          id_tags,
+          image: newPicture,
+        }
+      );
+        res.redirect('/sneakers/collection')
+    }catch(err){
+        next(err)
+    }
+})
 module.exports = router;
